bc3ef52486e46b857b96baba76d0256f33cac255
Laura Stassi <61417043+lauraspaghetti@users.noreply.github.com>
2025-01-29 14:15:41 +0000

    add first pages

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RecipeController extends AbstractController
+{
+    #[Route('/recipe', name: 'recipe.index')]
+    public function index(Request $request): Response
+    {
+        return new Response('Recipes');
+    }
+
+    #[Route('/recipe/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9-]+', 'id' => '\d+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return new Response ('Recipe: '.$slug);
+    }
+}
